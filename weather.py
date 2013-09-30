509520fa75bb780ecc43da209dac05393785e851
Andrea Stagi <stagi.andrea@gmail.com>
2013-09-30 15:06:08 +0200

    Working on res stuff, return the ceiling of temperature

--- weather.py
+++ weather.py
@@ -1,6 +1,5 @@
-import requests
+import requests, math
 from flask import Flask, request, jsonify
-
 from metwit import Metwit
 
 app = Flask(__name__)
@@ -13,8 +12,8 @@ error_codes = {
 @app.route("/weather", methods = ['POST'])
 def weather():
     """
-    INPUT BUNDLE: {'0': latitude, '1': longitude, '2': unit (f or c)}
-    E.G. {'0': 439596, '1': 109551, '2': 'f'}
+    INPUT BUNDLE: {'0': latitude, '1': longitude, '2': unit ("F" or "C")}
+    E.G. {'0': 439596, '1': 109551, '2': 'F'}
 
     OUTPUT BUNDLE: {'0': error code, '1': weather condition, '2': temperature}
     E.G. {'0': 0, '1': 1, '2': 78}
@@ -23,7 +22,7 @@ def weather():
     latitude = float(request.json['0']) / 10000
     longitude = float(request.json['1']) / 10000
 
-    format = request.json.get('2', 'c')
+    format = request.json.get('2', 'C')
 
     results = Metwit.weather.get(location_lat=latitude, location_lng=longitude)
 
@@ -35,12 +34,11 @@ def weather():
 
         # temperature is in Kelvin
 
-        if format == 'c':
+        if format == 'C':
             temperature = temperature - 273.15
         else:
             temperature = temperature * 9 / 5.0 - 459.67
-
-        return jsonify( {'0': 0, '1': 'TODO', '2': int(temperature)})
+        return jsonify( {'0': 0, '1': 'TODO', '2': int(math.ceil(temperature))})
     else:
         error_code = error_codes['NO_RESULTS']
 
